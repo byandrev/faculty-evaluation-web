958c86672433e80383e9005f70ab5c25ace3a551
Andres Parra <byandrev@gmail.com>
2026-01-24 16:35:29 -0500

    feat: create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import Analysis from "./components/Analysis";
 import Feedback from "./components/Feedback";
+import Header from "./components/Header";
 
 export function App() {
+  return (
+    <div>
+      <Header />
 
-  return <div>
-    <header className="p-4 bg-gray-800 text-white text-center text-xl font-bold">
-      Feedback Analyzer
-    </header>
-
-    <div className="p-6 grid gap-4
-  md:grid-cols-2
-  ">
-    <Feedback />
-    <Analysis />
-  </div>
-  </div>
-
+      <div className="p-6 grid gap-4 md:grid-cols-2 container mx-auto">
+        <Feedback />
+        <Analysis />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { MessageSquareText } from "lucide-react";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b">
+      <div className="container flex h-14 items-center px-4 mx-auto">
+        <div className="flex items-center gap-2 font-semibold mr-4">
+          <MessageSquareText className="h-6 w-6" />
+
+          <span className="hidden sm:inline-block text-lg">
+            Feedback Analyzer
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
