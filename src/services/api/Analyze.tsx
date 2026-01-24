341072349af83927d55495d07d3b7b493905c14b
Andres Parra <byandrev@gmail.com>
2026-01-24 17:23:08 -0500

    feat: update analyzeComment function to return an array of AnalysisResults

--- src/services/api/Analyze.tsx
+++ src/services/api/Analyze.tsx
@@ -2,7 +2,7 @@ import { API_URL } from "../../config";
 
 import type { AnalysisResults } from "../../context/ResultsContext";
 
-async function analyzeComment(comment: string): Promise<AnalysisResults> {
+async function analyzeComment(comment: string): Promise<AnalysisResults[]> {
   const request = await fetch(`${API_URL}/comments/`, {
     method: "POST",
     headers: {
@@ -17,7 +17,7 @@ async function analyzeComment(comment: string): Promise<AnalysisResults> {
 
   const data = await request.json();
 
-  return data;
+  return [data];
 }
 
 export default analyzeComment;
