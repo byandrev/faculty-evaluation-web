9cb1d46dae66b20d6cd6723f3400f48836ca739e
Andres Parra <byandrev@gmail.com>
2026-01-24 17:21:58 -0500

    feat: add CSV upload functionality to the API

---
+++ src/services/api/Upload.ts
@@ -0,0 +1,23 @@
+import { API_URL } from "../../config";
+
+import type { AnalysisResults } from "../../context/ResultsContext";
+
+async function uploadCsv(file: File): Promise<AnalysisResults[]> {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const request = await fetch(`${API_URL}/upload/`, {
+    method: "POST",
+    body: formData,
+  });
+
+  if (!request.ok) {
+    throw new Error("Failed to upload CSV");
+  }
+
+  const data = await request.json();
+
+  return data.results;
+}
+
+export default uploadCsv;
